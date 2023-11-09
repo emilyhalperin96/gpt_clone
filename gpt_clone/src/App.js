bf20dfd76460cebe43487e1b820b8345b0c11eda
emilyphalperin@gmail.com <emilyhalperin@Emilys-MacBook-Pro-2.local>
2023-11-09 16:53:06 -0500

    skeleton with CSS

--- gpt_clone/src/App.js
+++ gpt_clone/src/App.js
@@ -2,15 +2,34 @@ const App = () => {
 
   return (
     <div className="app">
-      <section className='sidebar'>
+      <section className='side-bar'>
         
         <button> + New chat</button>
-        <ul className='history'></ul>
+        <ul className='history'>
+          <li>test</li>
+        </ul>
         <nav>
           <p>made by Emily</p>
         </nav>
       </section>
-      <section className='main'></section>
+      <section className='main'>
+        <h1>GPT Clone</h1>
+        <ul className='feed'>
+        
+        </ul>
+        <div className='bottom-section'>
+          <div className='input-container'>
+            <input></input>
+            <div id='submit'></div>
+          </div>
+          <p className='info'>
+            Chat GPT Mar 14 Version. Free Research Preview. 
+            Our goal is to make AI systems more natural and safe to interact with.
+            Your feedback will help us improve.
+          </p>
+
+        </div>
+      </section>
     </div>
   );
 }
