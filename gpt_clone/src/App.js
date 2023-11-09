63935323d0eeb936cacbc8e962d0afde1ff9201a
emilyphalperin@gmail.com <emilyhalperin@Emilys-MacBook-Pro-2.local>
2023-11-09 16:35:02 -0500

    set up project

---
+++ gpt_clone/src/App.js
@@ -0,0 +1,18 @@
+const App = () => {
+
+  return (
+    <div className="app">
+      <section className='sidebar'>
+        
+        <button> + New chat</button>
+        <ul className='history'></ul>
+        <nav>
+          <p>made by Emily</p>
+        </nav>
+      </section>
+      <section className='main'></section>
+    </div>
+  );
+}
+
+export default App;
